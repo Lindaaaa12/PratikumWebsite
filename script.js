8b5d5f912873c3eff05ebc6d3b8873636fdd67c3
Lindaaaa12 <lindasar0102@gmail.com>
2024-10-09 01:26:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
-// Fungsi untuk menampilkan hasil pesanan di dalam console
-function tampilkanPesanan() {
-  // Mengambil semua checkbox yang dipilih
-  const checkboxes = document.querySelectorAll('input[name="menu"]:checked');
-  
-  let pesanan = [];
-  let totalHarga = 0;
+function submitOrder() {
+    const form = document.getElementById('orderForm');
+    const selectedFoods = Array.from(form.food)
+        .filter(input => input.checked)
+        .map(input => input.value);
 
-  // Looping setiap checkbox yang dipilih
-  checkboxes.forEach((checkbox) => {
-    const menu = checkbox.value;
-    const harga = parseInt(checkbox.getAttribute('data-price'));
-
-    // Menambahkan item pesanan ke array pesanan dan menghitung total harga
-    pesanan.push(menu);
-    totalHarga += harga;
-  });
-
-  // Menampilkan hasil pesanan di console
-  if (pesanan.length > 0) {
-    console.log("Anda memesan:", pesanan.join(", "));
-    console.log("Total harga: Rp", totalHarga);
-  } else {
-    console.log("Silakan pilih setidaknya satu menu.");
-  }
+    console.log('Pesanan:', selectedFoods);
+    alert('Pesanan Anda: ' + selectedFoods.join(', '));
 }
-
 
 
    
